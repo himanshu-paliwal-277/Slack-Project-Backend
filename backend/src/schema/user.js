b7962692ddf33681e3b7f809c767b2a475c9ed46
Himanshu Paliwal <hpaliwal364@gmail.com>
2025-08-22 09:19:21 +0530

    added schema layer

---
+++ backend/src/schema/user.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: [true, 'Username already exists'],
+      minLength: [3, 'Username must be at least 3 characters'],
+      match: [
+        /^[a-zA-Z0-9]+$/,
+        'Username must contain only letters and numbers'
+      ]
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: [true, 'Email already exists'],
+      match: [
+        // eslint-disable-next-line no-useless-escape
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please fill a valid email address'
+      ]
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required']
+    },
+    avatar: {
+      type: String
+    }
+  },
+  {
+    timeStamp: true
+  }
+);
+
+userSchema.pre('save', function saveUser(next) {
+  const user = this;
+  user.avatar = `https://robohash.org/${user.userName}`;
+  next();
+});
+
+const User = mongoose.model('users', userSchema);
+
+export default User;
